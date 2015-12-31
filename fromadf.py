bc8f1e899c148477630dd81a05d397e8912e9435
Nicholas FitzRoy-Dale <wzdd@lardcave.net>
2015-12-31 15:47:02 +0000

    Checkpoint with tweening
    Tweening is broken though, necessitates a change in file format

--- fromadf.py
+++ fromadf.py
@@ -13,155 +13,197 @@ Data format. View it as a finite-state machine, I guess.
 
 animations = (
 	# Initial hands move-in
-	0x13c6,
+	0x12bc,
 
 	# Intro picture-in-picture dancing girl with gun
-	0x2e3a,
+	0x2c00,
 
 	# Main intro (Bond girls)
-	0x10def,
+	0x10800,
 
 	# First dancer (38s - 47s)
-	0x68012,
+	0x67230,
 
-	# Second dancer (without cuts) (50s-53s)
-	0x75d7c,
+	# 4: Second dancer (without cuts) (50s-53s)
+	0x75c66,
 
 	# Third dancer (cut in with second) (51s-54s?)
-	0x770e0,
+	0x77016,
 
-	0x77934,
+	0x7789e,
 
-	0x781ca,
+	0x78104,
 
 	# ?? broken, hat
-	0x8d84e,
+	#0x8d840,
 
 	# Crazy-hips dancer, morphs to geometric shapes and other dancers, ending face spinning away and loading screen (1:02-1:10)
-	0x8dd4e,
+	0x8da00,
 
 	# Sequence after loading screen w/ crouching ending at 1:36
-	0x983b5,
+	# TODO
+	#0x983b5,
 
 	# Dancer with hat, cut in with previous.
-	0x996dc,
+	0x99546,
 
-	0x9af08,
+	0x9ae1a,
 
 	# jumping 
-	0x9da4e,
+	0x9d9c4,
 
-	0x9e28e,
+	0x9e1b8,
 
-	0x9f3ec,
+	0x9f31a,
 
-	0xa05d8,
+	0xa0552,
 
-	0xa133c,
+	0xa12f2,
 
 	# "angley" distorted dancer (but not distorted?)
-	0xb05fb,
+	# TODO -- the index for this is FUBAR
+	#0xb05fb,
 
-	# dancer, unknown
-	0xb35be,
+	# dancer, unknown (suspiciously similar to the first one)
+	0xb3274,
 
 	# TODO: lots of image-looking stuff here, but doesn't obviously fit a pattern. Maybe it's that grid thing?
 
+	0xb989c,
+
 	# Dancer, various shape morphs (which do not morph here)
-	0xc0776,
+	0xc0374,
 
 	# dancer, various shape morphs (which do not morph here)
-	0xc4b97,
+	0xc472a,
 
 	# final two dancers, shown in close-up, drawn as outline in the demo, part 1 (male)
-	0xcbeb4,
+	0xcbd82,
 
 	# final two dancers, part 2 (male)
-	0xcd982,
+	0xcd850,
 
-	# Dancer, front view (TODO broken 0 portion)
-	0xcf53c,
+	# Dancer, front view 
+	0xcf46e,
 
 	# Final two dancers, part 3 (female)
-	0xd0c08,
+	0xd0b52,
 
 	# Final two dancers, part 4 (female)
-	0xd1930,
+	0xd1872,
 
 	# Arm outstretched, panning towards fingers and offscreen
-	0xd2796,
+	0xd2700,
 
 	# Dancer (flickering portion?)
-	0xd3d51,
+	0xd3c00,
 
 	# Dancer (flickering portion?) #2
-	0xd4732,
+	0xd4488,
 
 	# Dancer (flickering portion?) #3 (and end of ADF!)
-	0xd8504,
+	0xd82fa,
 )
-
-image = animations[0]
-
-
-path = '/home/wzdd/Downloads/Spaceballs-StateOfTheArt (1).adf'
 
 class ShapeMaker:
 	def __init__(self, handle):
 		self.handle = handle
 
 	def getscript(self):
-		script = []
+		script = {}
 
-		while True:
-			cmd = struct.unpack('B', self.handle.read(1))[0]
+		# Read the index first.
+		startpos = self.handle.tell()
+
+		num_indices = struct.unpack('>H', self.handle.read(2))[0]
+		indices = []
+
+		for i in range(num_indices + 1):
+			index = struct.unpack('>I', self.handle.read(4))[0]
+			indices.append(index)
+
+		# Read the script. Note that indices may repeat, hence the dict.
+		for index in indices:
+			pos = startpos + index
+			if index not in script:
+				print(hex(index))
+				self.handle.seek(pos)
+				#
+				# have to pass startpos down because tweens
+				# require it
+				script[index] = self.read_one(startpos)
+				if script[index] is None:
+					print("Early exit")
+					break
+
+		return {'indices': indices, 'frames': script}
+
+	def read_one(self, startpos):
+		cmd_major = struct.unpack('B', self.handle.read(1))[0]
+		script = [cmd_major]
+
+		if cmd_major not in (1, 2, 3, 4, 5, 6):
+			print("Unknown major cmd %x" % (cmd_major))
+			return None
+	
+
+		cmd_start = self.handle.tell()
+		for i in range(cmd_major):
+
+			cmd_minor = struct.unpack('B', self.handle.read(1))[0]
+			element = None
+
 			# TODO the 0 story is FUBAR
-			if cmd == 0xff:
+			if cmd_minor == 0xff:
 				# 0 = unknown. ff = likely end of frames
-				print('End of frames %x' % (cmd))
-				script.append((cmd, []))
-				break
-			elif cmd in (0, 1, 2, 3, 4, 5, 6):
-				script.append((cmd, []))
-			elif cmd == 7:
-				script.append((cmd, list(self.handle.read(2)))) # unknown
-			elif cmd == 8:
-				script.append((cmd, list(self.handle.read(45)))) # unknown
-			elif cmd == 0xc:
-				script.append((cmd, list(self.handle.read(10)))) # unknown
-			elif cmd == 0xe:
-				script.append((cmd, list(self.handle.read(9)))) # unknown
-			elif cmd == 0xf:
-				script.append((cmd, list(self.handle.read(1)))) # unknown
-			elif cmd in (0x27, 0x29):
-				script.append((cmd, list(self.handle.read(11)))) # unknown
-			elif cmd == 0x37:
-				# End of frames?
-				print("End of frames")
-				script.append((cmd, []))
-				break
-			elif cmd == 0x3f and self.handle.tell() == 0x01231a:
-				script.append((cmd, list(self.handle.read(49)))) # don't know what this is yet, collecting evidence.
-			elif cmd & 0xd0 == 0xd0:
-				self.get_vector(script, cmd)
-			elif cmd in (0x76, 0x78):
-				script.append((cmd, list(self.handle.read(4)))) # unknown
-			elif cmd == 0x92:
-				script.append((cmd, list(self.handle.read(7)))) # unknown
-			elif cmd == 0x96:
-				script.append((cmd, list(self.handle.read(14)))) # unknown
-			elif cmd in (0xe6, 0xe7, 0xf2):
-				script.append((cmd, list(self.handle.read(6)))) # unknown
-			elif cmd == 0xe8:
-				script.append((cmd, list(self.handle.read(7)))) # unknown
+				print('End of frames %x' % (cmd_minor))
+				element = (cmd_minor, [])
+			elif cmd_minor in (0, 1, 2, 3, 4, 5, 6):
+				element = (cmd_minor, [])
+			elif cmd_minor == 7:
+				element = (cmd_minor, list(self.handle.read(2))) # unknown
+			elif cmd_minor == 8:
+				element = (cmd_minor, list(self.handle.read(45))) # unknown
+			elif cmd_minor == 0xc:
+				element = (cmd_minor, list(self.handle.read(10))) # unknown
+			elif cmd_minor == 0xe:
+				element = (cmd_minor, list(self.handle.read(9))) # unknown
+			elif cmd_minor == 0xf:
+				element = (cmd_minor, list(self.handle.read(1))) # unknown
+			elif cmd_minor in (0x27, 0x29):
+				element = (cmd_minor, list(self.handle.read(11))) # unknown
+			elif cmd_minor == 0x3f and self.handle.tell() == 0x01231a:
+				element = (cmd_minor, list(self.handle.read(49))) # don't know what this is yet, collecting evidence.
+			elif cmd_minor & 0xd0 == 0xd0:
+				element = self.get_vector(cmd_minor)
+			elif cmd_minor in (0x76, 0x78):
+				element = (cmd_minor, list(self.handle.read(4))) # unknown
+			elif cmd_minor == 0x92:
+				element = (cmd_minor, list(self.handle.read(7))) # unknown
+			elif cmd_minor == 0x96:
+				element = (cmd_minor, list(self.handle.read(14))) # unknown
+			elif cmd_minor == 0xe6:
+				# Tweening -- probably also applies to e7
+				relative_to = cmd_start - startpos 
+				idx_from, idx_to, current_step, total_steps = struct.unpack('>HHBB', self.handle.read(6))
+				# Turns out that these shapes can refer to INSIDE PORTIONS of draw-multiple commands! Aargh...
+				# have to change file format.
+				element = (cmd_minor, (str(relative_to - idx_from), str(relative_to + idx_to), current_step, total_steps))
+			elif cmd_minor in (0xe7, 0xf2):
+				element = (cmd_minor, list(self.handle.read(6))) # unknown
+			elif cmd_minor == 0xe8:
+				element = (cmd_minor, list(self.handle.read(7))) # unknown
 			else:
-				print("Unknown cmd %02x @%06x" % (cmd, self.handle.tell() - 1))
-				break
+				print("Unknown cmd %02x @%06x" % (cmd_minor, self.handle.tell() - 1))
 
-		print("Final index", hex(self.handle.tell()))
+			if element is not None:
+				script.append(element)
+			else:
+				return None
+
 		return script
 
-	def get_vector(self, script, cmd):
+	def get_vector(self, cmd_minor):
 		position = self.handle.tell()
 
 		points = None
@@ -169,12 +211,6 @@ class ShapeMaker:
 		output_dict = {"position": '0x%x' % (position), "shape": []}
 
 		num_points = struct.unpack('B', self.handle.read(1))[0]
-		if(num_points == 0):
-			# this appears to be a reset.
-			script.append((cmd, output_dict))
-			script.append((0, []))
-			return
-
 		points = self.handle.read(num_points * 2)
 
 		# Load the shape
@@ -188,19 +224,34 @@ class ShapeMaker:
 		output_dict['shape'] = shape
 		print("Found vector at position %x" % (position))
 
-		script.append((cmd, output_dict))
+		return (cmd_minor, output_dict)
 
-with open(path, 'rb') as h:
-	h.seek(image)
+path = 'Spaceballs-StateOfTheArt.adf'
 
-	shapemaker = ShapeMaker(h)
+def write_animation(image):
+	filename = 'script%06x.json' % (image)
+	pathname = os.path.join(OUTPUT_DIR, filename)
 
-	filename = os.path.join(OUTPUT_DIR, 'script%06x.json' % (image))
-	script = shapemaker.getscript()
-	with open(filename, 'w') as h:
-		json.dump(script, h, sort_keys=True, indent=4)
+	with open(path, 'rb') as h:
+		h.seek(image)
+		shapemaker = ShapeMaker(h)
+
+		script = shapemaker.getscript()
+		with open(pathname, 'w') as h:
+			json.dump(script, h, sort_keys=True, indent=4)
+		
+		print("Wrote", pathname)
 	
-	print("Wrote", filename)
+	return filename
 
+def write_all_animations():
+	filenames = []
+	for image in animations:
+		filenames.append(write_animation(image))
+		# break # just the first for now
 
+	with open(os.path.join(OUTPUT_DIR, 'scripts.json'), 'w') as h:
+		json.dump(filenames, h)
+
+write_all_animations()
 
--- shapes/sota.js
+++ shapes/sota.js
@@ -1,6 +1,6 @@
 var canvas;
 var idxElem;
-var script = [];
+var script = null;
 var logtextbox;
 
 /* canvas bit-planes */
@@ -30,20 +30,28 @@ function setup() {
 
 	logtextbox = document.getElementById("commandstextbox");
 
-	loadScript("script0013c6.json");
+	//loadScript("script0012bc.json");
+	loadScript("script0c0374.json");
 }
 
-function log(idx, cmd, args) {
-	var shortargs;
+function log(idx, frame) {
+	var text = "idx: " + idx + " cmd: " + frame[0] + "\n";
 
-	if(args.hasOwnProperty("shape") && args["shape"].length > 5) {
-		shortargs = JSON.stringify({"position": args["position"],
-			"shape": args["shape"].slice(0, 5)}) + "[...]";
-	} else {
-		shortargs = JSON.stringify(args);
+	for(i = 1; i < frame.length; i++) {
+		var cmd = frame[i][0];
+		var args = frame[i][1];
+		var shortargs;
+
+		if(args.hasOwnProperty("shape")) {
+			shortargs = JSON.stringify({"position": args["position"],
+				"shape": args["shape"].slice(0, 5)}) + "[...] len=" + args["shape"].length;
+		} else {
+			shortargs = JSON.stringify(args);
+		}
+		text += "  sub:" + cmd + " args:" + shortargs + "\n";
 	}
 
-	logtextbox.value += idx + ": " + cmd + ": " + shortargs + "\n";
+	logtextbox.value += text;
 	logtextbox.scrollTop = logtextbox.scrollHeight;
 }
 
@@ -69,8 +77,7 @@ function draw_cmd_to_plane_idx(cmd) {
 	}
 }
 
-function draw(bitplane, shape_info, clearPlane) {
-	var shape = shape_info["shape"];
+function draw(bitplane, shape, clearPlane) {
 
 	// TODO combine bit-planes. Pretty difficult
 	// to simulate without actually using bit-planes :(
@@ -105,36 +112,43 @@ function draw(bitplane, shape_info, clearPlane) {
 	}
 }
 
-function draw_multiple(ctx, idx, count) 
+function draw_multiple(ctx, frame) 
 {
 	// Read the next count cmds, which should be draws
 	var drawn_in_plane = new Array(0, 0, 0, 0);
 
-	while(count) {
-		var cmd = script[idx][0];
-		var args = script[idx][1];
-		log(idx, cmd, args);
+	var num_parts = frame[0];
+
+	for(var i = 0; i < num_parts; i++) {
+		var part = frame[i + 1];
+		var cmd = part[0];
+		var args = part[1];
 
 		if(cmd >= 0xd0 && cmd <= 0xdf) {
 			// we know what these are.
 			var bitplane = draw_cmd_to_plane_idx(cmd);
 
-			draw(bitplane, args, drawn_in_plane[bitplane]==0);
+			draw(bitplane, args["shape"], drawn_in_plane[bitplane]==0);
 			drawn_in_plane[bitplane] = 1;
-			count --;
-		} else if (cmd == 0) {
-			// This appears to be a clear or reset which doesn't
-			// affect the draw count following.
-		} else if (cmd == 0xe6 || cmd == 0xe8) {
-			// We don't know what these are, but they don't
+		} else if (cmd == 0xe6) {
+			// Tweening
+			var tween_shape_from = args[0];
+			var tween_shape_to   = args[1];
+			var tween_t          = args[2]; // position in tween
+			var tween_count      = args[3];
+
+			// Construct a temporary shape by lerping from shape_from to shape_to.
+			var shape = lerp_tween(tween_shape_from, tween_shape_to, tween_t, tween_count, 0);
+			draw(0, shape, drawn_in_plane[0]==0);
+			drawn_in_plane[0] = 1;
+		} else if (cmd == 0xe8) {
+			// We don't know what this is, but they don't
 			// cause problems so don't clutter the log.
-			count --;
+			// Probably tweening in other bitplane.
 		} else {
 			console.log("unexpected draw cmd " + cmd);
 			console.log(args);
-			count --;
 		}
-		idx ++;
 	}
 
 	clear(ctx);
@@ -142,37 +156,49 @@ function draw_multiple(ctx, idx, count)
 	for(var i = 0; i < 4; i++) {
 		ctx.drawImage(bitplanes[i], 0, 0);
 	}
+}
 
-	return idx;
+function lerp_tween(tween_shape_from_idx, tween_shape_to_idx, tween_t, tween_count, bitplane_idx) {
+	// okay this is a little nasty
+	console.log(script["frames"][tween_shape_from_idx]);
+	var tween_shape_from = script["frames"][tween_shape_from_idx][bitplane_idx + 1][1]["shape"];
+	var tween_shape_to   = script["frames"][tween_shape_to_idx][bitplane_idx + 1][1]["shape"];
+	var new_shape = new Array();
+
+	// Point selection is wrong -- we should tween the points as well; if we go 10 to 20 we should
+	// pick (0, 0), (0, 1), (1, 2), (2, 2) etc
+	var points = Math.max(tween_shape_from.length, tween_shape_to.length);
+	var point_multiplier_from = tween_shape_from.length / points;
+	var point_multiplier_to   = tween_shape_to.length / points;
+
+	var amt = tween_t / tween_count;
+
+	for(var i = 0; i < points; i++) {
+		var point_from = Math.floor(i * point_multiplier_from);
+		var point_to   = Math.floor(i * point_multiplier_to);
+
+		var x = Math.floor(tween_shape_from[point_from][0] + (amt * (tween_shape_to[point_to][0] - tween_shape_from[point_from][0])));
+		var y = Math.floor(tween_shape_from[point_from][1] + (amt * (tween_shape_to[point_to][1] - tween_shape_from[point_from][1])));
+		new_shape.push([x, y]);
+	}
+
+	return new_shape;
+
 }
 
 function step() {
 	var idx = idxElem.value;
 
-	var cmd = script[idx][0];
-	var args = script[idx][1];
-	log(idx, cmd, args);
+	if(idx < script["indices"].length) {
+		var frame = script["frames"][script["indices"][idx]];
 
-	idx ++;
+		log(idx, frame);
 
-	var ctx = canvas.getContext("2d");
+		var ctx = canvas.getContext("2d");
+		draw_multiple(ctx, frame);
 
-	switch(cmd) {
-		case 0:
-		case 1: // one vector follows
-		case 2: // two vectors follow
-		case 3: // three vectors follow
-		case 4:
-		case 5:
-		case 6:
-			idx = draw_multiple(ctx, idx, cmd);
-			break;
-
-		default:
-			console.log(cmd + ": Unexpected cmd encountered");
-			break;
+		idx ++;
 	}
-
 	idxElem.value = idx;
 }
 
