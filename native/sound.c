3fe7ba537cbb9b5f5079c1cda42c21a2f577c7c1
Nicholas FitzRoy-Dale <nicholas@fitzroydale.org>
2017-12-26 12:08:09 +0000

    Support for SDL_Mixer 2 and libmodplug.

--- native/sound.c
+++ native/sound.c
@@ -2,103 +2,78 @@
 #define _BSD_SOURCE
 
 #include <stdbool.h>
-#include <mikmod.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <SDL2/SDL_mixer.h>
 
 #include "wad.h"
 #include "sound.h"
 #include "backend.h"
 
-#ifdef EMULATE_FMEMOPEN
-#include "fmemopen.h"
-#endif
-
-
 static bool nosound;
 
-static MODULE *current_mod;
+static Mix_Music *current_mod;
+static SDL_RWops *current_mod_rwops;
 int mod_file_idx;
 
-static SAMPLE *current_sample;
+static Mix_Chunk *current_sample;
+static SDL_RWops *current_sample_rwops;
 int snd_file_idx;
 
-FILE *file_open(int idx)
-{
+static SDL_RWops *_load(int idx) {
 	size_t size;
 
 	uint8_t *data = backend_wad_load_file(idx, &size);
 
-	if(data == NULL) {
-		return NULL;
-	} else {
-		return fmemopen(data, size, "r");
+	return SDL_RWFromMem(data, size);
+}
+
+static void _unload(SDL_RWops **ops) {
+	SDL_RWclose(*ops);
+	*ops = NULL;
+}
+
+static void _unload_mod()
+{
+	if(current_mod_rwops) {
+		_unload(&current_mod_rwops);
+		Mix_FreeMusic(current_mod);
 	}
 }
 
-static MODULE *wz_load_mod(int idx)
+static void _unload_sample()
 {
-	FILE *fp = file_open(idx);
-	if(fp == NULL)
-		return NULL;
-
-	MODULE *mod = Player_LoadFP(fp, 8, 0);
-
-	fclose(fp);
-
-	return mod;
-}
-
-static SAMPLE *wz_load_sample(int idx)
-{
-	FILE *fp = file_open(idx);
-	if(fp == NULL)
-		return NULL;
-
-	SAMPLE *sample = Sample_LoadFP(fp);
-
-	fclose(fp);
-
-	return sample;
+	if(current_sample_rwops) {
+		_unload(&current_sample_rwops);
+		Mix_FreeChunk(current_sample);
+	}
 }
 
 void sound_update(void)
 {
-	if(!nosound)
-		MikMod_Update();
+	if(!nosound) {
+	}
 }
 
 bool sound_init(bool arg_nosound)
 {
+	bool succeeded = true;
+
 	if(arg_nosound) {
 		nosound = true;
 		printf("Sound disabled by request.\n");
 	} else {
 		nosound = false;
-
-		MikMod_RegisterAllDrivers();
-		MikMod_RegisterLoader(&load_mod);
-
-		md_mode |= DMODE_SOFT_MUSIC; // wtf else did you put in my namespace, mikmod?
-		
-		if(MikMod_Init("")) {
-			fprintf(stderr, "sound_init: %s\n", MikMod_strerror(MikMod_errno));
-			return false;
-		}
-
-		current_mod  = NULL;
-		mod_file_idx = -1;
-
-		current_sample = NULL;
-		snd_file_idx = -1;
-
-		//snd_heartbeat = wz_load_sample(SND_HEARTBEAT_FN);
-		MikMod_SetNumVoices(-1, 1);
-
-		MikMod_EnableOutput();
+		bool succeeded;
+#ifdef __EMSCRIPTEN__
+		// Note: this requires a patched SDL_mixer currently
+		succeeded = Mix_OpenAudioDevice(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096, NULL, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE) != -1;
+#else
+		succeeded = Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096) != -1;
+#endif
 	}
 
-	return true;
+	return succeeded;
 }
 
 bool sound_mod_play(int new_mod_idx)
@@ -106,24 +81,27 @@ bool sound_mod_play(int new_mod_idx)
 	if(nosound)
 		return true;
 
-	if(new_mod_idx != mod_file_idx) {
-		if(current_mod)
-			Player_Free(current_mod);
+	Mix_HaltMusic();
 
-		current_mod = wz_load_mod(new_mod_idx);
+	if(new_mod_idx != mod_file_idx) {
+		if(current_mod_rwops) {
+			_unload_mod();
+		}
+
+		current_mod_rwops = _load(new_mod_idx);
+		current_mod = Mix_LoadMUS_RW(current_mod_rwops, 0);
 		mod_file_idx = new_mod_idx;
+		Mix_PlayMusic(current_mod, -1);
 	}
 
-	Player_Start(current_mod);
 	return true;
 }
 
 bool sound_mod_stop()
 {
-	if(nosound)
-		return true;
-
-	Player_Stop();
+	if(!nosound) {
+		Mix_HaltMusic();
+	}
 	return true;
 }
 
@@ -133,16 +111,15 @@ bool sound_sample_play(int sample_idx)
 		return true;
 
 	if(snd_file_idx != sample_idx) {
-		if(current_sample)
-			Sample_Free(current_sample);
+		if(current_sample) {
+			_unload_sample();
+		}
 
-		current_sample = wz_load_sample(sample_idx);
+		current_sample_rwops = _load(sample_idx);
+		current_sample = Mix_LoadWAV_RW(current_sample_rwops, 0);
 		snd_file_idx = sample_idx;
+		Mix_PlayChannel(-1, current_sample, 0);
 	}
-
-	int8_t voice = Sample_Play(current_sample, 0, SFX_CRITICAL);
-	Voice_SetVolume(voice, 255);
-	Voice_SetPanning(voice, PAN_CENTER);
 
 	return true;
 }
@@ -152,14 +129,10 @@ bool sound_deinit()
 	if(!nosound) {
 		sound_mod_stop();
 
-		if(current_sample)
-			Sample_Free(current_sample);
-
-		if(current_mod)
-			Player_Free(current_mod);
-
-		MikMod_Exit();
+		_unload_sample();
+		_unload_mod();
+		Mix_CloseAudio();
 	}
+
 	return true;
 }
-
